b7453bb493915aab0f96317ee77bbddc69a3605d
TKogure <kogure@izmotion.co.jp>
2014-12-26 16:17:36 +0900

    index下のゴミ削除

--- index.php
+++ index.php
@@ -128,20 +128,7 @@ get_header(); ?>
 			</section>
 
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php ugokie_paging_nav(); ?>
+			
 
 		<?php else : ?>
 
@@ -152,5 +139,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
