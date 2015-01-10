403392a207b2db246e077ef0d4995562bc4095ab
TKogure <kogure@izmotion.co.jp>
2015-01-10 16:00:04 +0900

    ニュース一覧

--- page-news.php
+++ page-news.php
@@ -11,18 +11,18 @@
  */
 
 get_header(); ?>
-<h2 class="performance">実績一覧</h2>
-
-		<?php query_posts('post_type=performance');
+<h2 class="news">ニュース</h2>
+			<dl>
+				<dt>2015年</dt>
+		<?php query_posts();
 		if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<dl  class="performanceslist">
-				<dt>
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(220,124)); ?></a>
-				</dt>
+
+
+
 				<dd>
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(220,124)); ?></a>
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					<p class="client"><?php the_field('クライアント',$post->ID); ?></p>
-					<p class="category"><span class="genre"><?php the_field('ジャンル',$post->ID); ?></span>&nbsp;&nbsp;<span class="plan"><?php the_field('プラン',$post->ID); ?></spn></p>
+					<?php the_time(Y年m月d日); ?>
 					<?php the_content( $post->post_content ) ?>
 				</dd>
 			</dl>
