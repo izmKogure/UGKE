3f917939aae9bafd2e9c3d449ef795d551e50e96
TKogure <tuan.dod@gmail.com>
2015-01-10 21:45:23 +0900

    ニュース一覧

--- content-single.php
+++ content-single.php
@@ -7,10 +7,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-			<?php ugokie_posted_on(); ?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -23,7 +19,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php ugokie_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- page-news.php
+++ page-news.php
@@ -11,22 +11,18 @@
  */
 
 get_header(); ?>
-<h2 class="news">ニュース</h2>
-			<dl>
-				<dt>2015年</dt>
+<h2 class="news-list">ニュース</h2>
+			<dl class="news-list">
+				<dt><p>2015年</p></dt>
 		<?php query_posts();
 		if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-
-				<dd>
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(220,124)); ?></a>
+			<dd>
 					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					<?php the_time(Y年m月d日); ?>
+					<p class="date"><?php the_time(Y年m月d日); ?></p>
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(220,124)); ?></a>
 					<?php the_content( $post->post_content ) ?>
 				</dd>
-			</dl>
 		<?php endwhile; endif; wp_reset_query(); ?>
-
+			</dl>
 
 <?php get_footer(); ?>
