0384c86a67b3532405ec7dc17e1327e7f1116287
TKogure <kogure@izmotion.co.jp>
2015-01-26 19:08:14 +0900

    like box変更

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ get_header(); ?>
 							js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&appId=758942267458537&version=v2.0";
 							fjs.parentNode.insertBefore(js, fjs);
 							}(document, 'script', 'facebook-jssdk'));</script>
-							<div class="fb-like-box" data-href="https://www.facebook.com/FacebookDevelopers" data-width="450" data-height="520" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+							<div class="fb-like-box" data-href="https://www.facebook.com/ugokie" data-width="450" data-height="520" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
 					</div>
 				</div>
 			</section>
