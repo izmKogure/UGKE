2922711c57077cc4be23125e552dc7654547d328
TKogure <kogure@izmotion.co.jp>
2015-01-26 14:13:22 +0900

    viewport

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=960, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 
 <?php wp_head(); ?>
